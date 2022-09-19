adf51d3f16e9de0d2066fe44e67f07961e850e4d
Kolyvanov <Kolyvanovsl80@mail.ru>
2022-09-19 23:35:23 +0300

    add txt-methods

--- src/main/java/org/exemple/magazin/Basket.java
+++ src/main/java/org/exemple/magazin/Basket.java
@@ -36,7 +36,7 @@ public class Basket implements Serializable {
 
     }
 
-    public void saveToJSON (Basket basket){
+    public void saveToJSON(Basket basket) {
         try {
             ObjectMapper mapper = new ObjectMapper();
             mapper.writeValue(new File("basket.json"), basket);
@@ -44,6 +44,65 @@ public class Basket implements Serializable {
             System.out.println("файл не может быть прочитан");
             throw new RuntimeException(e);
         }
+    }
+
+    public void saveTxt(File textFile) {
+        try (BufferedWriter wr = new BufferedWriter(new FileWriter(textFile))) {
+            for (String product : products) {
+                wr.write(product + "&");
+            }
+            wr.write("\n");
+            for (int price : prices) {
+                wr.write(price + "&");
+            }
+            wr.write("\n");
+            for (int count : countOfProducts) {
+                wr.write(count + "&");
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    public static Basket loadFromTxtFile(File textFile) {
+        Basket basket;
+        String[] products = new String[0];
+        int[] prices = new int[0];
+        int[] countOfProducts = new int[0];
+        try (BufferedReader br = new BufferedReader(new FileReader(textFile))) {
+            String[] s;
+            int lineNum = 0;
+            while (br.ready()) {
+                if (lineNum == 0) {
+                    products = br.readLine().split("&");
+                } else if (lineNum == 1) {
+                    s = br.readLine().split("&");
+                    prices = new int[s.length];
+                    for (int i = 0; i < s.length; i++) {
+                        prices[i] = Integer.parseInt(s[i]);
+                    }
+
+                } else {
+                    s = br.readLine().split("&");
+                    countOfProducts = new int[s.length];
+                    for (int i = 0; i < s.length; i++) {
+                        countOfProducts[i] = Integer.parseInt(s[i]);
+                    }
+                }
+                lineNum++;
+            }
+            basket = new Basket(products, prices);
+            for (int i = 0; i < countOfProducts.length; i++) {
+                basket.addToCart((i +  1), countOfProducts[i]);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+
+        }
+        return basket;
     }
 
 
